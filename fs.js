d1b5e7eb630f009b1c164824bfb98c9877605076
SharathgowdaDR <sharathgowdadr63>
2023-09-01 19:01:49 +0530

    fs

---
+++ fs.js
@@ -0,0 +1,47 @@
+const http = require('http');
+const fs = require('fs');
+const { URL } = require('url');
+
+const server = http.createServer((req, res) => {
+  const url = new URL(req.url, `http://${req.headers.host}`);
+
+  if (url === '/') {
+    if (req.method === 'GET') {
+      const messages = fs.readFileSync('messages.txt', 'utf8').split('\n').filter(Boolean);
+      res.write('Content-Type','text/html');
+      res.write(`<html><body><form id="messageForm" method="POST"><textarea name="message" placeholder="Type your message"></textarea>
+              <button type="submit">Submit</button></form>
+            <ul id="messageList">
+              ${messages.map(message => `<li>${message}</li>`).join('')}
+            </ul>
+          </body>
+        </html>
+      `);
+      res.end();
+    } else if (req.method === 'POST') {
+      let body = '';
+      req.on('data', chunk => {
+        body += chunk.toString();
+      });
+
+      req.on('end', () => {
+        const newMessage = new URLSearchParams(body).get('message');
+if (newMessage) {
+          const messages = fs.readFileSync('messages.txt', 'utf8').split('\n').filter(Boolean);
+          messages.unshift(newMessage);
+          fs.writeFileSync('messages.txt', messages.join('\n'));
+          res.write(302, { Location: '/' });
+          res.end();
+        } else {
+          res.write(400, { 'Content-Type': 'text/plain' });
+          res.write('Bad Request: No message provided');
+        }
+      });
+      res.end();
+    }
+  } else {
+    res.writeHead('Content-Type','text/plain');
+    res.end('Not Found');
+  }
+});
+server.listen(3000);
